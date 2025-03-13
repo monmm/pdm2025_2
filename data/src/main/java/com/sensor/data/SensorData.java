b89ae695da9dc7ce1c2c9b6e24e0cc297ad40fe0
monica <monmm@ciencias.unam.mx>
2025-03-13 12:05:51 -0600

    Reacomodo de datos temperatura y humedad

--- data/src/main/java/com/sensor/data/SensorData.java
+++ data/src/main/java/com/sensor/data/SensorData.java
@@ -1,20 +1,21 @@
 package com.sensor.data;
 
 public class SensorData {
-
+	
+	private String temperature;
 	private String humidity;
-    private String temperature;
 
-    public SensorData(String humidity, String temperature) {
-        this.humidity = humidity;
-        this.temperature = temperature;
-    }
-
-    public String getHumidity() {
-        return humidity;
+    public SensorData(String temperature, String humidity) {
+    	this.temperature = temperature;
+    	this.humidity = humidity;       
     }
 
     public String getTemperature() {
         return temperature;
     }
+    
+    public String getHumidity() {
+        return humidity;
+    }
+
 }
