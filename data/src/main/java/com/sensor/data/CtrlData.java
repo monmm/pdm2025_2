9e29b831d5d83b808d0fbcbf136d3ed7fda474dd
1rv1nn <1rv1n@ciencias.unam.mx>
2025-03-17 12:42:48 -0600

    Generación de datos random en temperatura y humedad

--- data/src/main/java/com/sensor/data/CtrlData.java
+++ data/src/main/java/com/sensor/data/CtrlData.java
@@ -1,5 +1,7 @@
 package com.sensor.data;
 
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/data")
 public class CtrlData {
+
+  private final Random random = new Random();
 	
 	@GetMapping
 	public SensorData getData() {
+    
 		// Datos Dummy random
-		SensorData data = new SensorData("XX", "YY");
+    String temperature = String.format("%.2f", random.nextDouble() * 50); 
+    String humidity = String.format("%.2f", random.nextDouble() * 100);  
 
-		return data;
+		return new SensorData(temperature, humidity);
+
 	}
 
 }
